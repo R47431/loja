9e09ae92fb1aa7c12ecdcfbc34f2e6ca4fd2272e
R47431 <joaorafael1223@hotmail.com>
2023-05-15 19:59:05 -0300

    criacao padrao

---
+++ produtos/src/main/java/com/loja/produtos/controller/ProdutoControler.java
@@ -0,0 +1,46 @@
+package com.loja.produtos.controller;
+
+import com.loja.produtos.model.Produto;
+import com.loja.produtos.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class ProdutoControler {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public Iterable<Produto> listar() {
+        return produtoService.lista();
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> Cadastrar(@RequestBody Produto produto) {
+        Produto cadastrarProduto = produtoService.criar(produto);
+        return ResponseEntity.ok(cadastrarProduto);
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> altera(@RequestBody Produto produto) {
+        Produto alteraProduto = produtoService.altera(produto);
+        return ResponseEntity.ok(alteraProduto);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Produto> deleta(@PathVariable Long id) {
+        Produto deletaProduto = produtoService.deleta(id);
+        return ResponseEntity.ok(deletaProduto);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Produto> buscaProduto(@PathVariable Long id) {
+        Produto buscaProduto = produtoService.buscaProduto(id);
+        return ResponseEntity.ok(buscaProduto);
+    }
+
+}
---
+++ produtos/src/main/java/com/loja/produtos/service/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.loja.produtos.service;
+
+import com.loja.produtos.model.Produto;
+import com.loja.produtos.repositorio.ProdutoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepositorio produtoRepositorio;
+
+    public Iterable<Produto> lista() {
+        return produtoRepositorio.findAll();
+    }
+
+    public Produto criar(Produto produto) {
+        return produtoRepositorio.save(produto);
+    }
+
+    public Produto altera(Produto produto) {
+        return produtoRepositorio.save(produto);
+    }
+
+    public Produto deleta(Long id) {
+        Produto produto = produtoRepositorio.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto não encontrado com o ID: " + id));
+
+        produtoRepositorio.deleteById(id);
+        return produto;
+    }
+
+    public Produto buscaProduto(Long id) {
+        return produtoRepositorio.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Produto não encontrado com o ID:" + id));
+    }
+}
