81f8789ba001cdbe1a7e540ca6206aba4488e8f2
R47431 <joaorafael1223@hotmail.com>
2023-05-27 18:57:50 -0300

    manipulando imagem nome da imagem vai ser o id do produto

--- produtos/src/main/java/com/loja/produtos/repositorio/ProdutoRepositorio.java
+++ produtos/src/main/java/com/loja/produtos/repositorio/ProdutoRepositorio.java
@@ -8,15 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface ProdutoRepositorio extends CrudRepository<Produto,Long> {
-
-    @Query("SELECT MAX(p.id) FROM Produto p")
-    Long obterUltimoId();
-
-    /*
-     @Query("SELECT p.nome FROM Produto p WHERE p.nome = :nome ORDER BY p.id DESC LIMIT 1")
-    String obterUltimoNome();
-     */
-
     Optional<Produto> findByNome( String nome);
 
 }
